e894e4531ac3ca055e8e1f85bc587dc382521edd
Pabitra Pabi <pabitraranjanrout@gmail.com>
2024-08-03 15:35:32 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider children={children} />
+        <SessionProvider>{children}</SessionProvider>
         <Toaster />
       </body>
     </html>
